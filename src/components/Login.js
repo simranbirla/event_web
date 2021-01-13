a72e1ae454ac3b9482c5d2ca44cdff1a833df1af
simranbirla <62006675+simranbirla@users.noreply.github.com>
2021-01-14 02:01:41 +0530

    login front page css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { signIn } from "../redux/actions";
 import { connect } from "react-redux";
-
-//import "../Style/Login.css";
+import "../Style/Login.css";
 const Login = (props) => {
   return (
-    <div className="login">
+    <div className="login_page">
       <h2>Login here through google</h2>
-
+      <img
+        src="https://img.freepik.com/free-vector/login-concept-illustration_114360-739.jpg?size=338&ext=jpg"
+        alt="login"
+      />
       <button onClick={props.signIn}>Login</button>
     </div>
   );
